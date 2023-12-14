0ce9250a8a2b80efe6f09433ea3b3735558e70aa
sashauly <sashauly.code@ya.ru>
2023-12-14 11:40:34 +0300

    add helpers

--- src/loader/Graph.cc
+++ src/loader/Graph.cc
@@ -19,11 +19,11 @@ void Graph::LoadGraphFromFile(const std::string& filename) {
   int size = 0;
   file >> size;
 
-  matrix_.resize(size, std::vector<u_int>(size));
+  adjMatrix_.resize(size, std::vector<u_int>(size));
 
   for (int i = 0; i < size; ++i) {
     for (int j = 0; j < size; ++j) {
-      file >> matrix_[i][j];
+      file >> adjMatrix_[i][j];
     }
   }
 
@@ -31,24 +31,36 @@ void Graph::LoadGraphFromFile(const std::string& filename) {
 }
 
 void Graph::ExportGraphToDot(const std::string& filename) {
-  std::ofstream file(filename);
+  std::ofstream dotFile(filename);
 
-  if (!file.is_open()) {
+  if (!dotFile.is_open()) {
     throw std::runtime_error("Could not open file");
   }
 
-  file << "graph {\n";
+  size_t size = GetSize();
 
-  for (size_t i = 0; i < matrix_.size(); ++i) {
-    for (size_t j = 0; j < matrix_.size(); ++j) {
-      if (matrix_[i][j] != 0) {
-        file << i << " -- " << j << ";\n";
+  dotFile << "graph {" << std::endl;
+
+  for (size_t i = 0; i < size; ++i) {
+    for (size_t j = 0; j < size; ++j) {
+      if (adjMatrix_[i][j] != 0) {
+        dotFile << i << " -- " << j << ";\n";
       }
     }
   }
 
-  file << "}\n";
+  dotFile << "}" << std::endl;
 
-  file.close();
+  dotFile.close();
+}
+
+void Graph::print() {
+  size_t size = GetSize();
+  for (size_t i = 0; i < size; i++) {
+    for (size_t j = 0; j < size; j++) {
+      std::cout << adjMatrix_[i][j] << " ";
+    }
+    std::cout << std::endl;
+  }
 }
 }  // namespace s21
--- src/loader/Graph.h
+++ src/loader/Graph.h
@@ -5,6 +5,7 @@
 #ifndef SIMPLENAVIGATOR_GRAPH_H
 #define SIMPLENAVIGATOR_GRAPH_H
 
+#include <iostream>
 #include <string>
 #include <vector>
 
@@ -15,14 +16,23 @@ class Graph {
  public:
   Graph() = default;
 
-  const matrix_t& GetMatrix() const { return matrix_; }
-  void SetMatrix(const matrix_t& matrix) { matrix_ = matrix; }
+  void addEdge(int src, int dest, int weight) {
+    adjMatrix_[src][dest] = weight;
+  }
+  void deleteEdge(int src, int dest) { adjMatrix_[src][dest] = 0; }
+
+  size_t GetSize() { return adjMatrix_.size(); }
+
+  const matrix_t& GetMatrix() const { return adjMatrix_; }
+  void SetMatrix(const matrix_t& matrix) { adjMatrix_ = matrix; }
 
   void LoadGraphFromFile(const std::string& filename);
   void ExportGraphToDot(const std::string& filename);
 
+  void print();
+
  private:
-  matrix_t matrix_{};
+  matrix_t adjMatrix_{};
 };
 
 }  // namespace s21
